78b654aecf63da0beaf23ce77ed07a827432e9e5
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-10-09 12:12:59 -0300

    password works

--- back-end/src/routes/signUpRoute.js
+++ back-end/src/routes/signUpRoute.js
@@ -4,7 +4,7 @@ import { getDbConnection } from '../db';
 
 export const signUpRoute = {
 	path: '/api/signup',
-	method: post,
+	method: 'post',
 	handler: async (req, res) => {
 		const { email, password } = req.body;
 
@@ -15,21 +15,39 @@ export const signUpRoute = {
 			res.sendStatus(409);
 		}
 
-		const passwordHash = await bcrypt.hash(password, 10);
-
-		const startingInfo = {
-			hairColor: '',
-			favoriteFood: '',
-			bio: '',
-		};
-
-		const result = await db.collection('users').insertOne({
-			email,
-			passwordHash,
-			info: startingInfo,
-			isVerified: false,
+		const passwordHash = await bcrypt.hash(password, bcrypt.genSaltSync(10), null, async (err, hash) => {
+			req.body.password = hash;
+			await console.log(hash);
+			const result = await db.collection('users').insertOne({
+				email,
+				password: hash,
+				info: startingInfo,
+				isVerified: false,
+			});
+			const startingInfo = {
+				hairColor: '',
+				favoriteFood: '',
+				bio: '',
+			};
+	
+			const { insertedId } = result;
+	
+			jwt.sign({
+				id: insertedId,
+				email,
+				info: startingInfo,
+				isVerified: false,
+			},
+			process.env.JWT_SECRET,
+			{
+				expiresIn: '2d',
+			},
+			(err, token) => {
+				if(err) {
+					return res.status(500).send(err);
+				}
+				res.status(200).json({ token });
+			});	
 		});
-
-		const { insertedId } = result;
 	}
 }
