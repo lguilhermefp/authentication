95f554385db2201c1a774011b612b26f3e03fcab
luis guilherme <luis-guilherme.pereira@unesp.br>
2021-10-10 19:17:49 -0300

    sign up added to front end

--- front-end/src/pages/SignUpPage.js
+++ front-end/src/pages/SignUpPage.js
@@ -1,7 +1,11 @@
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { useToken } from '../auth/useToken';
 
 export const SignUpPage = () => {
+	const [token, setToken] = useToken();
+
 	const [errorMessage, setErrorMessage] = useState('');
 	const [emailValue, setEmailValue] = useState('');
 	const [passwordValue, setPasswordValue] = useState('');
@@ -10,7 +14,14 @@ export const SignUpPage = () => {
 	const history = useHistory();
 
 	const onSignUpClicked = async() => {
-		alert('log in not implemented yet')
+		const response = await axios.post('/api/signup', {
+			email: emailValue,
+			password: passwordValue,
+		});
+		
+		const { token } = response.data;
+		setToken(token);
+		history.push('/');
 	}
 
 	return (
@@ -18,18 +29,19 @@ export const SignUpPage = () => {
 			<h1>Sign Up</h1>
 			{errorMessage && <div className="fail">{errorMessage}</div>}
 			<input
+				type="text"
 				value={emailValue}
-				onchange={e => setEmailValue(e.target.value)}
+				onChange={e => setEmailValue(e.target.value)}
 			 	placeholder="someone@gmail.com" />
 			<input
 				type="password"
 				value={passwordValue}
-				onchange={e => setPasswordValue(e.target.value)}
+				onChange={e => setPasswordValue(e.target.value)}
 				placeholder="password" />
 			<input
 				type="password"
 				value={confirmPasswordValue}
-				onchange={e => setConfirmPasswordValue(e.target.value)}
+				onChange={e => setConfirmPasswordValue(e.target.value)}
 				placeholder="confirm password" />
 			<hr />
 			<button
@@ -37,7 +49,7 @@ export const SignUpPage = () => {
 					!emailValue || !passwordValue ||
 					passwordValue  !== confirmPasswordValue
 				}
-				onclick={onSignUpClicked}>sign Up</button>
+				onClick={onSignUpClicked}>sign Up</button>
 			<button onClick={() => history.push('/login')}>Already have an account? Log In</button>
 		</div>
 	)
